3f6fe1c3b4ec77292ea8a899010788b646976af9
Antonio Gabriel <biepe07@gmail.com>
2026-02-10 20:27:33 -0300

    Adcionando modelo das transacoes

--- dindintrack/models.py
+++ dindintrack/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Transacao(models.Model):
+    descricao = models.CharField(max_length=255)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data = models.DateField()
+
+    def __str__(self):
+        return f"{self.descricao} - R${self.valor} em {self.data}"
