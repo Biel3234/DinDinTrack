cdcc321d9cb5b37358c491e46d7635fa2f339c5f
Antonio Gabriel <biepe07@gmail.com>
2026-02-11 19:36:10 -0300

    Alterando modelo das transacoes

--- dindintrack/models.py
+++ dindintrack/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Transacao(models.Model):
     TIPOS_TRANSACAO = (
         ('entrada', 'Entrada'),
         ('saida', 'Saída'),
     )
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tarefas', null=False)
     descricao = models.CharField(max_length=255)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
     tipo = models.CharField(choices=TIPOS_TRANSACAO)
