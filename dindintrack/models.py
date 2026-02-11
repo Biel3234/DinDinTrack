d62dab3c0da28981bf845b819699e16a78bb06d9
Antonio Gabriel <biepe07@gmail.com>
2026-02-10 21:15:54 -0300

    Atualizando modelo das transacoes

--- dindintrack/models.py
+++ dindintrack/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 class Transacao(models.Model):
+    TIPOS_TRANSACAO = (
+        ('entrada', 'Entrada'),
+        ('saida', 'Saída'),
+    )
     descricao = models.CharField(max_length=255)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
-    data = models.DateField()
+    tipo = models.CharField(choices=TIPOS_TRANSACAO)
+    data = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.descricao} - R${self.valor} em {self.data}"
