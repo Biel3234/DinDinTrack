86526197aea4f74fc869ebaf54d3908d81391eb2
Antonio Gabriel <biepe07@gmail.com>
2026-02-11 19:37:49 -0300

    Feito o sistema de lancar saida e entrada das transacoes

--- dindintrack/urls.py
+++ dindintrack/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from dindintrack import views
 
 urlpatterns = [
-    ...
+    path('controle/', views.ControleFinanceiro.as_view(), name='render_create')
 ]
--- dindintrack/views.py
+++ dindintrack/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from .models import Transacao
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class ControleFinanceiro(View):
+    def get(self, request):
+        transacoes = Transacao.objects.all()
+        return render(request, 'tela_principal.html', {'transacoes': transacoes})
+    
+    def post(self, request):
+        usuario = self.request.user
+        descricao = request.POST.get('descricao')
+        valor = request.POST.get('valor')
+        tipo = request.POST.get('tipo')
+
+        Transacao.objects.create(
+            usuario = usuario,
+            descricao = descricao,
+            valor = valor,
+            tipo = tipo,
+        )
+
+        return redirect('render_create')
+
