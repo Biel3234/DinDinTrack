36806124bce8a180807825ff07cc31e7b4f5c753
Antonio Gabriel <biepe07@gmail.com>
2026-02-19 20:03:11 -0300

    Feito o sistema de login e encerrar sessao

--- dindintrack/urls.py
+++ dindintrack/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from dindintrack import views
 
 urlpatterns = [
-    path('controle/', views.ControleFinanceiro.as_view(), name='render_create')
+    path('controle/', views.ControleFinanceiro.as_view(), name='render_create'),
+    path('login/', views.fazer_login, name='login' ),
+    path('logout/', views.encerrar_login, name='deslogar'),
 ]
--- dindintrack/views.py
+++ dindintrack/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render, redirect
 from django.views.generic import View
 from .models import Transacao
 from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse
 
 
-class ControleFinanceiro(View):
+class ControleFinanceiro(LoginRequiredMixin, View):
+    login_url = 'login'
     def get(self, request):
         perfil = self.request.user
         transacoes = perfil.transacoes.all()
@@ -38,4 +42,23 @@ class ControleFinanceiro(View):
         )
 
         return redirect('render_create')
+    
+def fazer_login(request):
+    if request.method == 'GET':
+        return render(request, 'tela_login.html')
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        password = request.POST.get('password')
 
+        user = authenticate(request, username = name, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('render_create')
+        else:
+            return HttpResponse(render(request, 'tela_login_erro.html'))
+        
+def encerrar_login(request):
+    logout(request)
+    return redirect('login')
+            
